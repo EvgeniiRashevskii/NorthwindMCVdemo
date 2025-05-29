a2355ec662db86270db8ea6dd985b0396823a606
eugenyrashevsky <rashevskii221@gmail.com>
2025-05-29 18:42:27 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindMCVdemo.Models;
+
+namespace NorthwindMCVdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            TilausDB3Entities2 db = new TilausDB3Entities2();
+
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.UserName;
+               return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+
+
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); 
+
+        }
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            ViewBag.LoggedStatus = "In";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhtiön Perustietojen kuvailua";
+                ViewBag.Herja = "Ole huolellinen, niin ei tule virhettä";
+                return View();
+            }            
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                ViewBag.Message = "Yhteystietojamme";
+                ViewBag.Herja = Session["UserName"];
+                return View();
+            }
+        }
+    }
+}
