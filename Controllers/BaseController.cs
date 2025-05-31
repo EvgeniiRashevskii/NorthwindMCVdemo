7ace10aa708c79965302f7a895fa3a2fc046791f
eugenyrashevsky <rashevskii221@gmail.com>
2025-05-31 16:00:45 +0300

    Loi sisäänkirjautumisen yleisohjaimen

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindMCVdemo.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["UserName"] == null)
+            {
+                filterContext.Result = RedirectToAction("Login", "Logins");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
